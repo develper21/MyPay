96e42b9886a4d028b4787ff1b315f6c7b4bccfaf
develper21 <githubnarvin@gmail.com>
2025-12-02 21:50:06 +0530

    Fix ESLint: disable requireConfigFile for config files to prevent Babel parsing errors

--- MyPayApp/.eslintrc.js
+++ MyPayApp/.eslintrc.js
@@ -3,13 +3,14 @@ module.exports = {
   extends: '@react-native',
   overrides: [
     {
-      files: ['jest.config.js', '.eslintrc.js'],
+      files: ['jest.config.js', '.eslintrc.js', 'babel.config.js'],
       env: {
         node: true,
       },
       parserOptions: {
         sourceType: 'script',
         ecmaVersion: 2020,
+        requireConfigFile: false,
       },
       rules: {
         'no-undef': 'off',
